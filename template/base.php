ee574bc5528da01f0d6eeb0d5e85a954f8d2b5b6
Pietro Ventrucci <pietro.ventrucci@studio.unibo.it>
2023-12-27 19:31:32 +0100

    Use Bootstrap for post

--- template/base.php
+++ template/base.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="it">
+<html lang="en">
 
 <head>
     <title>
@@ -8,6 +8,10 @@
     <meta charset="UTF-8" />
     <link href="css/style.css" type="text/css" rel="stylesheet" />
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+          crossorigin="anonymous">
 </head>
 
 <body>
@@ -23,6 +27,9 @@
         <p>SnapSpark</p>
         <p>Carabini Luca, Ventrucci Pietro</p>
     </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+            crossorigin="anonymous"></script>
 </body>
 
 </html>
--- template/lista-post.php
+++ template/lista-post.php
@@ -1,24 +1,22 @@
-<?php if(isset($templateparams["titolo_pagina"])) : ?>
-    <h2>
-        <?php echo $templateparams["titolo_pagina"]; ?>
-    </h2>
-<?php else : ?>
-    <h2>Articoli</h2>
-<?php endif; ?>
-
-
 <?php foreach ($templateparams["posts"] as $post) : ?>
-    <article>
-        <header>
-            <h2>Titolo</h2>
-            <p><?php echo $post["username"]; ?> - <?php echo $post["data"] ?></p>
-            <img src="<?php echo POST_FOLDER . $post["file"]; ?>" alt="">
+    <article class="card col-10 mx-auto">
+
+        <header class="card-header">
+            <img src="<?php echo AVATAR_FOLDER . "avatar.png"; ?>" class="me-2" alt="">
+            <?php echo $post["username"]; ?> - <?php echo $post["data"] ?>
         </header>
-        <section>
-            <p><?php echo $post["descrizione"]; ?></p>
-        </section>
-        <footer>
-            <p><?php echo $post["spark"] , " spark"; ?></p>
+
+        <img src="<?php echo POST_FOLDER . $post["file"]; ?>" alt="">
+        <div class="card-body">
+            <p class="card-text">
+                <?php echo $post["descrizione"]; ?>
+            </p>
+        </div>
+
+        <footer class="card-footer">
+            <p class="card-text">
+                <?php echo $post["spark"], " spark"; ?>
+            </p>
         </footer>
     </article>
 <?php endforeach; ?>
