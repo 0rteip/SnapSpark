c122074e910f0db9aba20ff8a6d7da643c81711f
Luca Carabini <luca02c2@gmail.com>
2023-12-29 17:13:05 +0100

    Add bar conditions

--- template/base.php
+++ template/base.php
@@ -3,7 +3,7 @@
 
 <head>
     <title>
-        <?= $templateParams["titolo"];  ?>
+        <?= $templateParams["titolo"];?>
     </title>
     <meta charset="UTF-8" />
     <link href="css/style.css" type="text/css" rel="stylesheet" />
@@ -15,41 +15,23 @@
     <nav class="navbar bd-navbar sticky-top bg-info" aria-label="User-navbar">
         <div class="container-fluid">
             <a class="navbar-brand fw-bold" href="index.php">SnapSpark</a>
-            <form class="d-flex" role="search">
-                <input class="form-control bg-info-subtle" type="search" placeholder="Search" aria-label="Search">
-            </form>
+            <?php   if($templateParams["nome"] == "template/lista-post.php") { 
+                        require("search-bar.php");
+                    }
+            ?>
         </div>
     </nav>
 
     <div class="container-xxl bd-gutter mt-3 my-md-4 bd-layout">
-
         <main>
             <?php require($templateParams["nome"]); ?>
         </main>
-
     </div>
 
-    <nav role="navigation" class="navbar navbar-expand bd-navabr sticky-bottom bg-info" aria-label="Main-navbar">
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav nav-justified w-100">
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php"><i class="fa fa-home"></i></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#"><i class="fa fa-search"></i></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#"><i class="fa fa-plus-square"></i></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#"><i class="fa fa-heart"></i></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="login.php"><i class="fa fa-user"></i></a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    <?php   if($templateParams["nome"] == "template/lista-post.php") { 
+                require("nav-bar.php");
+            }
+    ?>
 
     <footer class="bd-footer py-4 py-md-5 bg-body-tertiary">
         <div class="container py-4 py-md-5 px-4 px-md-3 text-body-secondary">
---
+++ template/search-bar.php
@@ -0,0 +1,4 @@
+
+<form class="d-flex" role="search">
+    <input class="form-control bg-info-subtle" type="search" placeholder="Search" aria-label="Search">
+</form>
