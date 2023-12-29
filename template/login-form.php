e4999c451793d82ae950862f487396a29cdaa9cb
Luca Carabini <luca02c2@gmail.com>
2023-12-29 12:18:32 +0100

    Create login page

---
+++ template/login-form.php
@@ -0,0 +1,14 @@
+<form action="#" method="post">
+
+    <div class="col mb-4">
+      <label for="exampleFormControlInput1" class="form-label">Email address</label>
+      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    </div>
+    
+    <label for="inputPassword5" class="form-label">Password</label><input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
+    <div class="col-12">
+        <button class="btn btn-primary" type="submit">Submit form</button>
+        <a href="new-account.php">Inscriviti se non hai un account!</a>
+    </div>
+    
+</form>
