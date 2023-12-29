2dcb6f6228bbc999c71cd957da923d9431932111
Luca Carabini <luca02c2@gmail.com>
2023-12-29 16:58:52 +0100

    add session

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 require_once "db/database.php";
-
+require_once "utils/functions.php";
+session_start();
 $dbh = new DatabaseHelper("localhost", "root", "", "SnapSpark", 3306);
 define("POST_FOLDER", "./img/");
 define("AVATAR_FOLDER", "./img/avatar/");
