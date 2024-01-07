8fd4b942dbc692eb1b8b46cf619e729d0dc1f587
Luca Carabini <luca02c2@gmail.com>
2024-01-07 23:28:42 +0100

    No relevant mod.

--- js/prova.js
+++ js/prova.js
@@ -1,12 +1,23 @@
 console.log("prova.js loaded");
 
-function clickBt(bt) {
-    document.getElementById('info').innerHTML = "<ul> <?php foreach($templateParams[" + bt.getAttribute('id') + "] as $info) : ?> <li><p><?php echo $info['info'] ?></p></li><?php endforeach;?> </ul>";
-} 
+function change(id) {
+    document.querySelectorAll('section').forEach(
+        section => {
+            if (section.getAttribute('class') !== id) {
+                section.style.display = 'none';
+            } else {
+                section.style.display = '';
+            }
+        }
+    );
+}
 
 const bts = document.querySelectorAll('input');
 bts.forEach(bt => {
     console.log(bt.getAttribute('id'));
-    bt.addEventListener("click", clickBt(bt));
-});
-console.log(document.getElementById('info'))
+    bt.addEventListener("click", function() {
+        id =bt.getAttribute('id');
+        change(id);
+        console.log("click");
+    });
+})
