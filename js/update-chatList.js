44ae597d3b29c101bd0717e826533df1e100e007
Luca Carabini <luca02c2@gmail.com>
2024-01-15 18:56:35 +0100

    Change update method for chat and messages

--- js/update-chatList.js
+++ js/update-chatList.js
@@ -7,9 +7,9 @@ function showElenco(result) {
             <div class="user-card">
                     <img alt="" class="avatar" id="chat-avatar" src="${result.avatar}avatar.png">
                     <div class="card custom-card borderless-card" id="chat-c">
-                    <div class="info-card">
+                    <div class="info-chat">
                         <div class="user-name">${chat.user}</div>
-                        <div class="card-text">${chat.testo}</div>
+                        <div class="card-text" data="${chat.data}">${chat.testo}</div>
                     </div>
                     </div>
             </div>
@@ -17,6 +17,34 @@ function showElenco(result) {
         `
     })
     document.getElementById('chat-container').innerHTML = chats;
+}
+function getCurrentChats() {
+    let chats = document.querySelectorAll('div.info-chat');
+    let result = [];
+    chats.forEach(chat => {
+        let user = chat.querySelector('div.user-name').innerHTML;
+        let info = chat.querySelector('div.card-text');
+        let tmp = {user:user, testo:info.innerHTML, data:info.getAttribute('data')};
+        result.push({user:user, testo:info.innerHTML, data:info.getAttribute('data')});
+    })
+    return result;
+}
+
+function chatsUpdate() {
+    
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "utils/search.php");
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function () {
+        let result = JSON.parse(this.responseText);
+        if (JSON.stringify(result.chats) !== JSON.stringify(getCurrentChats()) && document.getElementById('search-bar-chats').value.length === 0) {
+            console.log('aggiorno')
+            showElenco(result);
+        }
+    }
+    xhr.send("string=" + "&type=3");
+    setTimeout('chatsUpdate()',1000);
 }
 
 function chatSearch(string, type) {
@@ -29,6 +57,6 @@ function chatSearch(string, type) {
         showElenco(result);
     }
     xhr.send("string=" + string + "&type=" + type );
-    setTimeout('chatSearch("", 3)',1000);
 }
-chatSearch("", 3);
+
+chatsUpdate();
--- template/chat-v.php
+++ template/chat-v.php
@@ -4,7 +4,7 @@
             <button type="button" class="btn btn-primary" onclick="location.href='new-chat.php'">Create New Chat</button>
         </div>
         <div class="col-3">
-            <input class="form-control bg-main-color-subtle" type="search" placeholder="Search" aria-label="Search" onkeyup="chatSearch(this.value, 3)">
+            <input id="search-bar-chats" class="form-control bg-main-color-subtle" type="search" placeholder="Search" aria-label="Search" onkeyup="chatSearch(this.value, 3)">
         </div>
     </div>
 </div>
