58ea75438d208c2a6f225be607523fc526dc652d
Luca Carabini <luca02c2@gmail.com>
2024-01-16 18:59:50 +0100

    Change Chat method

--- utils/search.php
+++ utils/search.php
@@ -1,7 +1,24 @@
 <?php
-function createChat()
-{
-    require_once '../bootstrap.php';
+function getChats($messages) {
+    require '../bootstrap.php';
+    $chats = array();
+    foreach($messages as $message) :
+        $user = "";
+        if ($message['sender'] !== $_SESSION['username']) {
+            $user = $message['sender'];
+
+        } else if($message['reciver'] !== $_SESSION['username']) {
+            $user=$message['reciver'];
+        }
+        $check = checkPresence($chats, $user);
+        $userInfo = $dbh->getUserInfo($user)["profile_img"];
+        $newChat = array('user' => $user, 'testo' => $message['testo'], 'data' => $message['data'], 'img' => $userInfo);
+        if ($check !== false) {
+            unset($chats[$check]);
+        }
+            array_push($chats, $newChat);
+    endforeach;
+    return array_reverse($chats);
 }
 function normalSearch($string, $type)
 {
