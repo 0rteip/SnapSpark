754038b9bf37d10ac14cc0f25e1ff6545db929a6
Luca Carabini <luca02c2@gmail.com>
2024-01-16 19:00:13 +0100

    Start creation of new way, for follow and unfollow

--- utils/segui.php
+++ utils/segui.php
@@ -10,8 +10,23 @@ function getFollowed()
     echo json_encode(array('followed' => $result));
 }
 
+function follow() {
+
+}
+
+function unfollow() {
+
+}
+
 if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest') {
     // Chiamata Ajax rilevata, esegui la funzione
+    if(isset($_POST["action"]) && isset($_POST['follow'])) {
+        if ($_POST["action"] == 'Follow') {
+            follow();
+        } else if($_POST["action"] == 'Unfollow') {
+            unfollow();
+        }
+    }
     getFollowed();
 } else {
     echo 'Accesso non consentito.';
