ab9348fd7916e69a616aaf8adbb4cf740bbdf4c8
Pietro Ventrucci <pietro.ventrucci@studio.unibo.it>
2024-01-17 02:06:49 +0100

    Add form control class

--- template/chat-v.php
+++ template/chat-v.php
@@ -2,7 +2,7 @@
     <div class="row row-cols-2 justify-content-center">
         <div class="col-sm-10">
             <label for="search-bar:-3" class="visually-hidden">Search chats</label>
-            <input id="search-bar:-3" class="search-bar bg-main-color-subtle" type="search" placeholder="Search chat" aria-label="Search">
+            <input id="search-bar:-3" class="form-control search-bar bg-main-color-subtle" type="search" placeholder="Search chat" aria-label="Search">
         </div>
         <div class="col-sm-2 text-end">
             <button type="button" id="newChatBt" class="btn btn-primary">New Chat</button>
