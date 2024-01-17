63725a3dcbbbfdcc405142091506da11b7ed6bb4
Pietro Ventrucci <pietro.ventrucci@studio.unibo.it>
2024-01-17 02:02:26 +0100

    Add label

--- template/follow-follower.php
+++ template/follow-follower.php
@@ -6,7 +6,9 @@
     <a class="nav-link <?php echo checkRadio($templateParams['action'], 'followed') ?>" href="info-follower.php?username=<?php echo $username ?>&action=followed" target="_self">Followed</a>
   </li>
 </ul>
-<input id="search-follower:-2:-<?php print($templateParams['username']); ?>:-<?php print($templateParams['action']); ?>" class="search-bar bg-main-color-subtle" name="follower-search" type="search" placeholder="Search" aria-label="Search" >
+
+<label for="search-user" class="visually-hidden">Search user</label>
+<input id="search-follower:-2:-<?php print($templateParams['username']); ?>:-<?php print($templateParams['action']); ?>" class="search-bar bg-main-color-subtle" name="follower-search" type="search" placeholder="Search" aria-label="Search">
 
 <div class="search">
   <?php require("list-username.php"); ?>
