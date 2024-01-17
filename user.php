621d193cee55d1a46e60a388857ab27c2b63591b
Luca Carabini <luca02c2@gmail.com>
2024-01-17 01:37:46 +0100

    Fixed bugs

--- user.php
+++ user.php
@@ -8,12 +8,9 @@ if (!isUserLoggedIn()) {
 $templateParams["titolo"] = "SnapSpark - Profile";
 $templateParams["hashtag"] = $dbh->getDailyHashtag();
 $templateParams["nome"] = "template/user-profile.php";
-
+$username = $_SESSION['username'];
 if (isset($_GET['username'])) {
     $username = $_GET['username'];
-} else {
-    $username = $_SESSION['username'];
-    $templateParams["requireCropper"] = true;
 }
 
 $posts = $dbh->getPostsByAuthor($username);
