e0c0829cfd618a9b40475e3840b1c45ad11baf31
Pietro Ventrucci <pietro.ventrucci@studio.unibo.it>
2024-01-22 17:31:16 +0100

    Add label

--- template/create-chat.php
+++ template/create-chat.php
@@ -1,6 +1,7 @@
+<label for="search-bar:-0" class="visually-hidden">Search a user</label>
 <input id="search-bar:-0" class="form-control search-bar bg-main-color-subtle" name="search" type="search" placeholder="Search" aria-label="Search">
 
-<section class="search">
+<div class="search">
     <?php foreach ($templateParams['sug'] as $sug) : ?>
 
         <a href="chat.php?reciver=<?php echo $sug['username'] ?>" class="reciver" sender="<?php echo $_SESSION['username'] ?>" reciver="<?php echo $sug['username'] ?>">
@@ -15,5 +16,5 @@
             </div>
         </a>
     <?php endforeach; ?>
-</section>
+</div>
 <script src="js/search.js"></script>
