edf8b408ecadd5a5d579751f650397e811247e09
luca <luca02c2@gmail.com>
2024-02-03 15:54:46 +0100

    Add send mail functions

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 require_once "db/database.php";
 require_once "utils/functions.php";
-
+require_once 'utils/mail.php';
 session_start();
 $dbh = new DatabaseHelper("localhost", "root", "", "SnapSpark", 3306);
 
---
+++ utils/mail.php
@@ -0,0 +1,29 @@
+<?php 
+function sendEmail($reciver, $sender, $type) {
+    $from = "luca02c2@gmail.com";
+    $subject = getMailObj($type);
+    $message = getMailText($sender, $type);
+    $headers = ["From" => $from];
+    mail($reciver, $subject, $message, $headers);
+}
+
+function getMailText($sender, $type) {
+    $init = $sender;
+    switch ($type) {
+        case 'send': return $init . " ti ha inviato un messaggio";
+        case 'removeMessage': return $init . " ha eliminato un messaggio";
+        case 'Follow': return  $init . " ha iniziato a seguirti";
+        case 'Unfollow': return $init . " ha smesso di seguirti";
+        case 'New' : return "Ciao, benvenuto nella nostra famiglia!";
+    }
+}
+
+function getMailObj($type) {
+    switch ($type) {
+        case 'send': return "Nuovo messaggio!";
+        case 'removeMessage': return "Eliminazione Messaggio";
+        case 'Follow': return "Nuovo follower!";
+        case 'Unfollow': return "Unfollow";
+        case 'New' : return "Benvenuto su SnapSpark!";
+    }
+}
